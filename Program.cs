fe0ff269024550ed59fd4b91bc90e1ec245c16fd
Philo Nordlund <philon@microsoft.com>
2022-04-29 07:54:11 -0700

    Add project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+
+using (var connection = await ConnectionMultiplexer.ConnectAsync(redisConnectionString))
+{
+    var cache = connection.GetDatabase();
+    var key = "keyName";
+
+    var value = await cache.StringGetAsync(key);
+    Console.WriteLine($"{key} BEFORE set: {value}");
+
+    var result = await cache.StringSetAsync(key, "value");
+
+    value = await cache.StringGetAsync(key);
+    Console.WriteLine($"{key} AFTER set: {value}");
+}
